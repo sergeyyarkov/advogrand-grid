8c8866556147e372e6e84844f2b53e9a16299cab
Sergey <32025367+sergeyyarkov@users.noreply.github.com>
2019-08-20 18:59:07 +0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,59 @@
+// Слайдер Comments
+$('.comments').owlCarousel({
+    loop:false,
+    margin:20,
+    dots:false,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+            dots:true
+        },
+        480:{
+            items:2,
+            dots:true
+        },
+        800:{
+            items:3,
+            dots:true
+        },
+        1135:{
+            nav:true
+        }
+    }
+});
+// Слайдер Partners
+$('.partners').owlCarousel({
+    loop:false,
+    margin:20,
+    dots:false,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+            dots:true
+        },
+        445:{
+            items:1,
+            dots:true
+        },
+        450:{
+            items:2,
+            dots:true
+        },
+        670:{
+            items:2,
+            dots:true
+        },
+        675:{
+            items:3,
+            dots:true
+        },
+        1135:{
+            items:4,
+            nav:true
+        }
+    }
+});
+// Partner Cards
+// ...code
