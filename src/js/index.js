099a381eb0e3e257ee3f5c4e6198cc43e07d10f6
Sergey <32025367+sergeyyarkov@users.noreply.github.com>
2019-08-21 11:44:07 +0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,71 @@
 // Слайдер Comments
 $('.comments').owlCarousel({
-    loop:false,
-    margin:20,
-    dots:false,
-    nav:false,
-    responsive:{
-        0:{
-            items:1,
-            dots:true
+    loop: false,
+    margin: 20,
+    dots: false,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1,
+            dots: true
         },
-        480:{
-            items:2,
-            dots:true
+        480: {
+            items: 2,
+            dots: true
         },
-        800:{
-            items:3,
-            dots:true
+        800: {
+            items: 3,
+            dots: true
         },
-        1135:{
-            nav:true
+        1135: {
+            nav: true
         }
     }
 });
 // Слайдер Partners
 $('.partners').owlCarousel({
-    loop:false,
-    margin:20,
-    dots:false,
-    nav:false,
-    responsive:{
-        0:{
-            items:1,
-            dots:true
+    loop: false,
+    margin: 20,
+    dots: false,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1,
+            dots: true
         },
-        445:{
-            items:1,
-            dots:true
+        445: {
+            items: 1,
+            dots: true
         },
-        450:{
-            items:2,
-            dots:true
+        450: {
+            items: 2,
+            dots: true
         },
-        670:{
-            items:2,
-            dots:true
+        670: {
+            items: 2,
+            dots: true
         },
-        675:{
-            items:3,
-            dots:true
+        675: {
+            items: 3,
+            dots: true
         },
-        1135:{
-            items:4,
-            nav:true
+        1135: {
+            items: 4,
+            nav: true
         }
     }
 });
 // Partner Cards
-// ...code
+const itemsPartner = document.querySelectorAll('.item-partner');
+itemsPartner.forEach((partner) => {
+    partner.addEventListener('mouseover', () => {
+        const partnerInfo = partner.querySelector('.partner-info');
+        partnerInfo.style.display = 'block';
+        partnerInfo.classList.add('fade');
+        partner.addEventListener('mouseout', () => {
+            const partnerInfo = partner.querySelector('.partner-info');
+            partnerInfo.style.display = 'none';
+            partnerInfo.classList.remove('fade');
+        });
+    });
+});
